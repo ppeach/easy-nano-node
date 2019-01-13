4d8b02137fe4d0e32b740fbbad07a2dc187db3dc
bananodroid <41457266+bananodroid@users.noreply.github.com>
2019-01-13 21:44:12 +1100

    Update check_nanonode.sh

--- enn/check_nanonode.sh
+++ enn/check_nanonode.sh
@@ -2,5 +2,5 @@
 curl --connect-timeout 3 --max-time 5 -g -s -d '{ "action": "version" }' [::1]:7076 > /dev/null;
 if [ $? -ne 0 ]; then
     echo "$(date) - Restarting Nano Node..."
-    docker restart enn_nanonode_1
+    docker restart nanonode
 fi;
