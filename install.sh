c01f6244dfc5d71d683157ffc3d6af7fc29d6414
bananodroid <41457266+bananodroid@users.noreply.github.com>
2019-01-13 21:12:28 +1100

    update to beta

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ test -f "$(which git )" || { echo "Git is not installed"; exit 1; }
 echo "== Cloning installation"
 # if the directory already exists pull the repo
 # otherwise clone it
-git -C /opt/easy-nano-node pull || git clone https://github.com/nanotools/easy-nano-node.git /opt/easy-nano-node
+git -C /opt/easy-nano-node pull || git clone https://github.com/bananodroid/easy-betanano-node.git /opt/easy-nano-node
 
 echo "== Starting installation"
 # and fire!
